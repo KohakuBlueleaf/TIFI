c84b5735d57a9e54891274b76c34bc749149ba4c
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-05-22 18:57:30 +0800

    Add single prompt encode

--- diff_utils.py
+++ diff_utils.py
@@ -9,6 +9,39 @@ def load_tokenizers(model="stabilityai/stable-diffusion-xl-base-1.0"):
     return tokenizer, tokenizer_2
 
 
+def encode_prompts_single(
+    tokenizer: CLIPTokenizer,
+    tokenizer_2: CLIPTokenizer,
+    text_encoder: CLIPTextModel,
+    text_encoder_2: CLIPTextModel,
+    prompt: str,
+):
+    max_length = tokenizer.model_max_length
+
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
+    input_ids2 = tokenizer_2(prompt, return_tensors="pt").input_ids.to("cuda")
+
+    concat_embeds = []
+    for i in range(0, input_ids.shape[-1], max_length):
+        concat_embeds.append(text_encoder(input_ids[:, i : i + max_length])[0])
+
+    concat_embeds2 = []
+    pooled_embeds2 = []
+    for i in range(0, input_ids.shape[-1], max_length):
+        hidden_states = text_encoder_2(
+            input_ids2[:, i : i + max_length], output_hidden_states=True
+        )
+        concat_embeds2.append(hidden_states.hidden_states[-2])
+        pooled_embeds2.append(hidden_states[0])
+
+    prompt_embeds = torch.cat(concat_embeds, dim=1)
+    prompt_embeds2 = torch.cat(concat_embeds2, dim=1)
+    prompt_embeds = torch.cat([prompt_embeds, prompt_embeds2], dim=-1)
+    pooled_embeds2 = torch.mean(torch.stack(pooled_embeds2, dim=0), dim=0)
+
+    return prompt_embeds, pooled_embeds2
+
+
 def encode_prompts(
     tokenizer: CLIPTokenizer,
     tokenizer_2: CLIPTokenizer,
