10134ea47a8cb64def854f2fc0c3b22469d0a920
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-06-06 20:12:48 +0800

    Long video refinement

--- scripts/long_video.py
+++ scripts/long_video.py
@@ -1,12 +1,8 @@
 import os, sys
-import random
-from copy import deepcopy
 
 sys.path.append(".")
 
-import torch
-from torchvision.transforms.functional import to_tensor, to_pil_image
-from tqdm import tqdm
+from torchvision.transforms.functional import to_tensor
 from PIL import Image
 
 from tifi.pipe.temporal_inpainting import TemporalInpainting
@@ -29,7 +25,6 @@ pipeline = TemporalInpainting(
 )
 
 frames = []
-frame_imgs = []
 
 for frame_file in os.listdir(TEST_VID):
     frames.append(
@@ -40,19 +35,9 @@ for frame_file in os.listdir(TEST_VID):
         )
     )
     frames.append(None)
-    frame_imgs.append(
-        Image.open(os.path.join(TEST_VID, frame_file)).convert("RGB").resize((896, 512))
-    )
-    frame_imgs.append(None)
-frames = frames[120 : 120 + 217]
-frame_imgs = frame_imgs[120 : 120 + 217]
+frames = frames[:-1]
 
+tifi_out, optical_flow_out = pipeline(frames, steps=8, denoise_strength=0.6, cfg=7.0)
 
-tifi_out, optical_flow_out = pipeline(frames, steps=8, denoise_strength=0.4, cfg=7.0)
-
-for idx, (frame, org_frame) in enumerate(zip(tifi_out[0], frame_imgs)):
-    if idx % 2 == 1:
-        frame.save(f"vid/out/{FORMAT}/{idx:04}.png")
-    else:
-        # Can avoid some weird reference/numerical error
-        org_frame.save(f"vid/out/{FORMAT}/{idx:04}.png")
+for idx, (frame, org_frame) in enumerate(tifi_out[0]):
+    frame.save(f"vid/out/{FORMAT}/{idx:04}.png")
